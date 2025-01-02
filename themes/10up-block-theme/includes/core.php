5dec4beb0230713bef86192b14e880c4d5b51925
Fabian Kägy <mail@fabian-kaegy.de>
2025-01-02 10:11:48 +0100

    updates

--- themes/10up-block-theme/includes/core.php
+++ themes/10up-block-theme/includes/core.php
@@ -15,19 +15,14 @@ use function TenupBlockTheme\Utility\get_asset_info;
  * @return void
  */
 function setup() {
-	$n = function ( $function_name ) {
-		return __NAMESPACE__ . "\\$function_name";
-	};
-
+	add_action( 'init', 'TenupBlockTheme\Core\scripts' );
+	add_action( 'init', 'TenupBlockTheme\Core\register_all_icons', 10 );
 	add_action( 'after_setup_theme', 'TenupBlockTheme\Core\i18n' );
 	add_action( 'after_setup_theme', 'TenupBlockTheme\Core\theme_setup' );
-	add_action( 'init', 'TenupBlockTheme\Core\scripts' );
-	add_action( 'wp_enqueue_scripts', 'TenupBlockTheme\Core\styles' );
-	add_action( 'enqueue_block_editor_assets', 'TenupBlockTheme\Core\editor_style_overrides' );
 	add_action( 'wp_head', 'TenupBlockTheme\Core\js_detection', 0 );
 	add_action( 'wp_head', 'TenupBlockTheme\Core\scrollbar_detection', 0 );
-	add_action( 'wp_head', 'TenupBlockTheme\Core\embed_ct_css', 0 );
-	add_action( 'init', 'TenupBlockTheme\Core\register_all_icons', 10 );
+	add_action( 'wp_enqueue_scripts', 'TenupBlockTheme\Core\styles' );
+	add_action( 'enqueue_block_editor_assets', 'TenupBlockTheme\Core\editor_style_overrides' );
 }
 
 /**
@@ -160,31 +155,5 @@ function js_detection() {
  * @return void
  */
 function scrollbar_detection() {
-	echo '<script>window.addEventListener("DOMContentLoaded",()=>{const t=()=>window.innerWidth-document.body.clientWidth;const e=()=>{document.documentElement.style.setProperty("--wp--custom--scrollbar-width",`${t()}px`)};e();window.addEventListener("resize",e);});</script>' . "\n";
-}
-
-/**
- * Inlines ct.css in the head
- *
- * Embeds a diagnostic CSS file written by Harry Roberts
- * that helps diagnose render blocking resources and other
- * performance bottle necks.
- *
- * The CSS is inlined in the head of the document, only when requesting
- * a page with the query param ?debug_perf=1
- *
- * @link https://csswizardry.com/ct/
- * @return void
- */
-function embed_ct_css() {
-
-	$debug_performance = rest_sanitize_boolean( filter_input( INPUT_GET, 'debug_perf', FILTER_SANITIZE_NUMBER_INT ) );
-
-	if ( ! $debug_performance ) {
-		return;
-	}
-
-	wp_register_style( 'ct', false ); // phpcs:ignore
-	wp_enqueue_style( 'ct' );
-	wp_add_inline_style( 'ct', 'head{--ct-is-problematic:solid;--ct-is-affected:dashed;--ct-notify:#0bce6b;--ct-warn:#ffa400;--ct-error:#ff4e42}head,head [rel=stylesheet],head script,head script:not([src])[async],head script:not([src])[defer],head script~meta[http-equiv=content-security-policy],head style,head>meta[charset]:not(:nth-child(-n+5)){display:block}head [rel=stylesheet],head script,head script~meta[http-equiv=content-security-policy],head style,head title,head>meta[charset]:not(:nth-child(-n+5)){margin:5px;padding:5px;border-width:5px;background-color:#fff;color:#333}head ::before,head script,head style{font:16px/1.5 monospace,monospace;display:block}head ::before{font-weight:700}head link[rel=stylesheet],head script[src]{border-style:var(--ct-is-problematic);border-color:var(--ct-warn)}head script[src]::before{content:"[Blocking Script – " attr(src) "]"}head link[rel=stylesheet]::before{content:"[Blocking Stylesheet – " attr(href) "]"}head script:not(:empty),head style:not(:empty){max-height:5em;overflow:auto;background-color:#ffd;white-space:pre;border-color:var(--ct-notify);border-style:var(--ct-is-problematic)}head script:not(:empty)::before{content:"[Inline Script] "}head style:not(:empty)::before{content:"[Inline Style] "}head script:not(:empty)~title,head script[src]:not([async]):not([defer]):not([type=module])~title{display:block;border-style:var(--ct-is-affected);border-color:var(--ct-error)}head script:not(:empty)~title::before,head script[src]:not([async]):not([defer]):not([type=module])~title::before{content:"[<title> blocked by JS] "}head [rel=stylesheet]:not([media=print]):not(.ct)~script,head style:not(:empty)~script{border-style:var(--ct-is-affected);border-color:var(--ct-warn)}head [rel=stylesheet]:not([media=print]):not(.ct)~script::before,head style:not(:empty)~script::before{content:"[JS blocked by CSS – " attr(src) "]"}head script[src][src][async][defer]{display:block;border-style:var(--ct-is-problematic);border-color:var(--ct-warn)}head script[src][src][async][defer]::before{content:"[async and defer is redundant: prefer defer – " attr(src) "]"}head script:not([src])[async],head script:not([src])[defer]{border-style:var(--ct-is-problematic);border-color:var(--ct-warn)}head script:not([src])[async]::before{content:"The async attribute is redundant on inline scripts"}head script:not([src])[defer]::before{content:"The defer attribute is redundant on inline scripts"}head [rel=stylesheet][href^="//"],head [rel=stylesheet][href^=http],head script[src][src][src^="//"],head script[src][src][src^=http]{border-style:var(--ct-is-problematic);border-color:var(--ct-error)}head script[src][src][src^="//"]::before,head script[src][src][src^=http]::before{content:"[Third Party Blocking Script – " attr(src) "]"}head [rel=stylesheet][href^="//"]::before,head [rel=stylesheet][href^=http]::before{content:"[Third Party Blocking Stylesheet – " attr(href) "]"}head script~meta[http-equiv=content-security-policy]{border-style:var(--ct-is-problematic);border-color:var(--ct-error)}head script~meta[http-equiv=content-security-policy]::before{content:"[Meta CSP defined after JS]"}head>meta[charset]:not(:nth-child(-n+5)){border-style:var(--ct-is-problematic);border-color:var(--ct-warn)}head>meta[charset]:not(:nth-child(-n+5))::before{content:"[Charset should appear as early as possible]"}link[rel=stylesheet].ct,link[rel=stylesheet][media=print],script[async],script[defer],script[type=module],style.ct{display:none}' );
+	echo '<script>window.addEventListener("DOMContentLoaded",()=>{const t=()=>window.innerWidth-document.body.clientWidth;const e=()=>{document.documentElement.style.setProperty("--wp--custom--scrollbar-width",`${t()}px`)};e();});</script>' . "\n";
 }
