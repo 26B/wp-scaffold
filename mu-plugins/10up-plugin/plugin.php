03bcfd84e8c02e8c14a46695f00821db36a78047
Daryll Doyle <daryll.doyle@10up.com>
2025-01-30 15:05:44 +0000

    Split the functional code into a couple of classes and update the init

--- mu-plugins/10up-plugin/plugin.php
+++ mu-plugins/10up-plugin/plugin.php
@@ -20,7 +20,7 @@
 define( 'TENUP_PLUGIN_VERSION', '0.1.0' );
 define( 'TENUP_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'TENUP_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
-define( 'TENUP_PLUGIN_INC', TENUP_PLUGIN_PATH . 'includes/' );
+define( 'TENUP_PLUGIN_INC', TENUP_PLUGIN_PATH . 'src/' );
 define( 'TENUP_PLUGIN_DIST_URL', TENUP_PLUGIN_URL . 'dist/' );
 define( 'TENUP_PLUGIN_DIST_PATH', TENUP_PLUGIN_PATH . 'dist/' );
 
@@ -41,13 +41,11 @@ if ( file_exists( TENUP_PLUGIN_PATH . 'vendor/autoload.php' ) ) {
 	require_once TENUP_PLUGIN_PATH . 'vendor/autoload.php';
 }
 
-// Include files.
-require_once TENUP_PLUGIN_INC . '/utility.php';
-require_once TENUP_PLUGIN_INC . '/core.php';
+$plugin_core = \TenUpPlugin\PluginCore::instance();
 
 // Activation/Deactivation.
-register_activation_hook( __FILE__, '\TenUpPlugin\Core\activate' );
-register_deactivation_hook( __FILE__, '\TenUpPlugin\Core\deactivate' );
+register_activation_hook( __FILE__, [ $plugin_core, 'activate' ] );
+register_deactivation_hook( __FILE__, [ $plugin_core, 'deactivate' ] );
 
 // Bootstrap.
-TenUpPlugin\Core\setup();
+$plugin_core->setup();
